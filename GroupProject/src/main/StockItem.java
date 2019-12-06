c86fef61d28d56d5eaa9969a8af223765c88e4a5
Jacob Allen <44875489+JacobA2000@users.noreply.github.com>
2019-12-06 13:09:52 +0000

    Added comments and slightly altered code to be more efficient

--- GroupProject/src/main/StockItem.java
+++ GroupProject/src/main/StockItem.java
@@ -75,67 +75,63 @@ public class StockItem {
      */
     
     private String fixedStockCode;
-    private int numInStock;
+    private int numInStock;    //Stock related variables
     private double itemPrice;
     
     String greenText = "\u001B[32m";
-    String yellowText = "\u001B[33m";
+    String purpleText = "\u001B[35m";   //Setting text colours
     String redText = "\u001B[31m";
     
     public boolean stockPositive = false;
-    public boolean stockPriceValid = false;
+    public boolean stockPriceValid = false;   //Declaring boolean flags used for validation
     public boolean quantityValid = false;
     
-    Scanner systemInput = new Scanner(System.in);
+    Scanner systemInput = new Scanner(System.in);    //Creating scanner for system input
     
     //STOCK ITEM CONSTRUCTOR
     public StockItem(String stockCode, int quantityInStock, double price){
         if(quantityInStock < 1){
             System.out.println(redText + "Cannot create stock with a quantity of less than 1");
-            quantityValid = false;
+            quantityValid = false;      //if quantity is less than 1 set quantity flag to false
         }
         else if (quantityInStock >= 1){
-            quantityValid = true;
+            quantityValid = true;     //if quantity greater than or equal to 1 set quantity flag to true.
         }
-        if(price <= 0.0){
+        if(price <= 0.00){
             System.out.println(redText + "Cannot create stock with a set price of less than 0.01");
-            stockPriceValid = false;
+            stockPriceValid = false;   //if price is less than or equal to 0.00 set stock price flag to false
         }
         else if (price > 0){
-            stockPriceValid = true;
+            stockPriceValid = true;   //if price is greater than 0 set stock price flag to true
         }
         if(quantityValid && stockPriceValid){
-            stockPositive = true;
-        }
-        if(stockPositive == true && stockPriceValid == true){
+            stockPositive = true;     //if both quantity and stock price flags are true, then set stockPositive flag to true and create the stock item
             this.fixedStockCode = stockCode;
             this.numInStock = quantityInStock;
             this.itemPrice = price;
-            System.out.println(yellowText + "Creating a stock with " + numInStock 
+            System.out.println(purpleText + "Creating a stock with " + numInStock 
                     + " units of unknown item, price of " + price + " each, and stock code " + fixedStockCode + "...");
         }
     }
     
     //SETTERS
     
-    /*NO SETTER FOR STOCK CODE AS IT IS FIXED*/
-    
     public void setQuantity(int quantity){
-        System.out.println(yellowText + "Updating quantity to new quantity of " + quantity + "...");
+        System.out.println(purpleText + "Updating quantity to new quantity of " + quantity + "...");
         numInStock = quantity;
     }
     
     public void setPrice(){
         double price;
-        System.out.println(yellowText + "Please enter the desired price: ");
-        price = systemInput.nextDouble();
+        System.out.println(purpleText + "Please enter the desired price: ");
+        price = systemInput.nextDouble();   //Getting users input and setting it as the price
         
-        if(price > 0.0){
-            System.out.println(yellowText + "Updating price to new price of " + price + "...");
-            itemPrice = price;
+        if(price > 0.0){ 
+            System.out.println(purpleText + "Updating price to new price of " + price + "...");
+            itemPrice = price;  //if the price is greater than 0 set the price to the inputted value.
         }
         else
-            System.out.println(redText + "Cannot set price less than 0.01");
+            System.out.println(redText + "Cannot set price less than 0.01");  //else output an error message
     }
     
     //GETTERS
@@ -160,7 +156,7 @@ public class StockItem {
     }
     
     public double getPriceVAT(){
-        return itemPrice + (itemPrice/100 * 17.5);
+        return itemPrice + (itemPrice/100 * getVAT());  //Add VAT to the price.
     }
     
     public double getVAT(){
@@ -169,51 +165,51 @@ public class StockItem {
     
     //STOCK MANIPULATION METHODS
     public void addStock(){
-        int incrementAmmount;
+        int incrementAmount;
         boolean incrementValid = true;
         boolean stockLevelValid = true;
         
-        System.out.println(yellowText + "Please enter an ammount of stock to add: ");
-        incrementAmmount = systemInput.nextInt();
+        System.out.println(purpleText + "Please enter an amount of stock to add: ");
+        incrementAmount = systemInput.nextInt(); //get the ammount of stock the user wishes to add.
         
-        if (incrementAmmount < 1) {
-            System.out.println(redText + "Cannot add stock, increment ammount is less than one.");
-            incrementValid = false;
+        if (incrementAmount < 1) {
+            System.out.println(redText + "Cannot add stock, increment amount is less than one.");    
+            incrementValid = false;     //if the ammount of stock to add is less than 1 set incrementValid flag to false.
         }
-        if (incrementAmmount + numInStock > 100){
+        if (incrementAmount + numInStock > 100){
             System.out.println(redText + "Cannot add stock, stock level is at max capacity.");
-            stockLevelValid = false;
+            stockLevelValid = false; //if the ammount of stock would exceed 100 after adding set stockLevelValid flag to false.
         }
         if (incrementValid && stockLevelValid){
-            System.out.println(yellowText + "Adding " + incrementAmmount + " units of stock...");
-            setQuantity(numInStock + incrementAmmount);
+            System.out.println(purpleText + "Adding " + incrementAmount + " units of stock...");
+            setQuantity(numInStock + incrementAmount);  //if both incrementValid and stockLevelValid flags are true, then update the quantity to reflect the addition of stock.
         }
     }
     
     public boolean sellStock(){
-        int decrementAmmount;
-        boolean decrementValid = true;
+        int decrementAmount;
+        boolean decrementValid = true;  
         boolean stockLevelValid = true;
         
-        System.out.println(yellowText + "Please enter an ammount of stock to sell: ");
-        decrementAmmount = systemInput.nextInt();
+        System.out.println(purpleText + "Please enter an amount of stock to sell: ");
+        decrementAmount = systemInput.nextInt();  //get the ammount the user wishes to sell.
         
-        if(decrementAmmount < 1){
-            System.out.println(redText + "Cannot sell stock, decrement ammount is less than one.");
-            decrementValid = false;
+        if(decrementAmount < 1){
+            System.out.println(redText + "Cannot sell stock, decrement amount is less than one.");
+            decrementValid = false; //if the ammount to sell is less than 1, then set the flag decrementValid to false;
         }
-        if(decrementAmmount > numInStock){
-            System.out.println(redText + "Cannot sell stock, decrement ammount is greater than quantity in stock.");
-            stockLevelValid = false;
+        if(decrementAmount > numInStock){
+            System.out.println(redText + "Cannot sell stock, decrement amount is greater than quantity in stock.");
+            stockLevelValid = false; //if the ammount to sell is greater than the amount in stock, then set the flag stockLevelValid to false;
         }
         
         if(decrementValid && stockLevelValid){
-            System.out.println(yellowText + "Sold " + decrementAmmount + " units of stock...");
-            setQuantity(numInStock - decrementAmmount);
-            return true;
+            System.out.println(purpleText + "Sold " + decrementAmount + " units of stock...");
+            setQuantity(numInStock - decrementAmount);  //if decrementValid and stockLevelValid flags are true then update the quantity to reflect the sale of stock.
+            return true; //returns true if sale has happened
         }
         else{
-            return false;
+            return false;  //returns false if sale has not happened
         }
     }
     
@@ -221,11 +217,11 @@ public class StockItem {
 
     public String toString(){
             String str;
-            System.out.println(yellowText + "Getting stock info...");
+            System.out.println(purpleText + "Getting stock info...");
             str = greenText + "-------------------STOCK INFO-------------------"
                 + greenText + "\nStock code:     " + getStockCode() 
                 + greenText + "\nStock name:     " + getStockName() 
-                + greenText + "\nStock desc:     " + getStockDesc() 
+                + greenText + "\nStock desc:     " + getStockDesc()                 //Use getter methods to get the relevant information to output.
                 + greenText + "\nUnits in stock: " + getQuantity() 
                 + greenText + "\nPrice:          " + getPrice() 
                 + greenText + "\nPrice with VAT: " + getPriceVAT()
@@ -234,12 +230,12 @@ public class StockItem {
             return str;
     }
     //NavSys Stock info
-    public static class NavSys extends StockItem{
+    public static class NavSys extends StockItem{                                      //Creating the NavSys class that is a subclass of StockItem
         public NavSys(String stockCode, int quantityInStock, double price) {
-            super(stockCode, quantityInStock, price);
+            super(stockCode, quantityInStock, price);  //Inherrit default values from the stock item class.
             
         }       
-        @Override
+        @Override //Overide StockItems getStockName, getStockDesc and toString method.
         public String getStockName(){
             return "Navigation System";
         }
@@ -251,7 +247,7 @@ public class StockItem {
         @Override
         public String toString(){
             String str;
-            System.out.println(yellowText + "Getting stock info...");
+            System.out.println(purpleText + "Getting stock info...");
             str = greenText + "-------------------STOCK INFO-------------------"
                 + greenText + "\nStock code:     " + getStockCode() 
                 + greenText + "\nStock name:     " + getStockName() 
@@ -282,7 +278,7 @@ public class StockItem {
         @Override
         public String toString(){
             String str;
-            System.out.println(yellowText + "Getting stock info...");
+            System.out.println(purpleText + "Getting stock info...");
             str = greenText + "-------------------STOCK INFO-------------------"
                 + greenText + "\nStock code:     " + getStockCode() 
                 + greenText + "\nStock name:     " + getStockName() 
@@ -313,7 +309,7 @@ public class StockItem {
         @Override
         public String toString(){
             String str;
-            System.out.println(yellowText + "Getting stock info...");
+            System.out.println(purpleText + "Getting stock info...");
             str = greenText + "-------------------STOCK INFO-------------------"
                 + greenText + "\nStock code:     " + getStockCode() 
                 + greenText + "\nStock name:     " + getStockName() 
@@ -344,7 +340,7 @@ public class StockItem {
         @Override
         public String toString(){
             String str;
-            System.out.println(yellowText + "Getting stock info...");
+            System.out.println(purpleText + "Getting stock info...");
             str = greenText + "-------------------STOCK INFO-------------------"
                 + greenText + "\nStock code:     " + getStockCode() 
                 + greenText + "\nStock name:     " + getStockName() 
